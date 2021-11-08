d23694283559a7dcfd8ac29ac4d18f97aa4e5e31
Domboy47 <dominiklutchman@hotmail.com>
2021-11-08 00:37:55 -0500

    Test Cases

--- Wall Factors/testCases.py
+++ Wall Factors/testCases.py
@@ -1,25 +1,28 @@
 ## Uses 7 test images and performs edge detection
-import Util as U
+import util as U
+import cv2
+import numpy as np
 
-for k in range(1,8):
-    # Loading image of wall
-    println("Testing Wall",k,":")
-    testImg = U.inputImage("Wall"+str(k)+".JPEG")
-
-    # Testing two different preprocessing algorithms
-    #   both have parameters to vary, will test multiple to see
-    #   optimal/most consistent value
-    meanSquareFilter, msfSuffix = msf(testImg,k)
-    msf_size = len(meanSquareFilter)
+def main():
+    for k in range(1,8):
+        # Loading image of wall
+        print("Testing Wall",k,":")
+        testImg = U.inputImage("Wall"+str(k)+".JPEG")
+        U.plotImage(testImg)
+        # Testing two different preprocessing algorithms
+        #   both have parameters to vary, will test multiple to see
+        #   optimal/most consistent value
+        meanSquareFilter, msfSuffix = msf(testImg,k)
+        msf_size = len(meanSquareFilter)
+        
+        gaussianBlur, blurSuffix = gBlur(testImg,k)
+        blur_size = len(gaussianBlur)
     
-    gaussianBlur, blurSuffix = gBlur(testImg,k)
-    blur_size = len(gaussianBlur)
-
-    tests = meanSquareFilter+gaussianBlur
-    testsSuffix = msfSuffix+blurSuffix
-
-    # Run edge detection algorithm
-    edgeDetect(tests,testsSuffix,msf_size,blur_size,k)
+        tests = meanSquareFilter+gaussianBlur
+        testsSuffix = msfSuffix+blurSuffix
+    
+        # Run edge detection algorithm
+        edgeDetect(tests,testsSuffix,msf_size,blur_size,k)
 
 
 # Testing Preprocessing
@@ -28,20 +31,20 @@ def msf(img,num):
     msf = []
     msfSuff = []
 
-    for i in range(30,40,10):
-        for j in range(10,30,10):
+    for i in range(10,40,10):
+        for j in range(10,40,10):
             #Image suffix for file naming
             print("Space Radius: ", i)
             print("Colour Radius: ", j)
             suffix = "msf"+str(i)+str(j)
-            msfSuff = []
-        
+            msfSuff.append(suffix)
+                    
             imgMSF = cv2.pyrMeanShiftFiltering(img,i,j)
-
+            
             U.saveImage(imgMSF, "MSF/wall"+str(num)+"_"+suffix)
             msf.append(imgMSF)
 
-    return msf
+    return msf, msfSuff
 
 def gBlur(img,num):
     print("Running Mean Shift Filtering")
@@ -54,11 +57,11 @@ def gBlur(img,num):
         suffix = "blur"+str(i)
         blurSuff.append(suffix)
 
-        imgMSF = U.Blur(testImg,i)
-        U.saveImage(imgMSF, "Blur/wall"+num+suffix)
+        imgBlur = U.Blur(img,i)
+        U.saveImage(imgBlur, "Blur/wall"+str(num)+suffix)
         gBlur.append(imgBlur)
             
-    return gBlur
+    return gBlur, blurSuff
 
 # Edge Detection
 def edgeDetect(imgArray,suffixArray,msfSize,gBlurSize,num):    
@@ -79,7 +82,7 @@ def edgeDetect(imgArray,suffixArray,msfSize,gBlurSize,num):
         Canny = U.Canny(Picture_Gray,T/2,T)
 
         # Contour
-        Contours_Outline = Wall.Find_Contours_Optimal(Canny,Picture,False)
+        Contours_Outline = U.Find_Contours_Optimal(Canny,Picture,False)
 
         U.saveImage(Contours_Outline,"edgeDetect/contour/Wall"+str(num)+"/"+suffixArray[i])
 
@@ -91,3 +94,6 @@ def edgeDetect(imgArray,suffixArray,msfSize,gBlurSize,num):
         
         U.saveImage(Contours_Filled_Moment,"edgeDetect/moment/Wall"+str(num)+"/"+suffixArray[i])
 
+
+
+main()
--- Wall Factors/util.py
+++ Wall Factors/util.py
@@ -1,12 +1,13 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 ## Opens image from "Climbing Photos" folder
 # string filePath = location of image to open
 # bool color = [0] for grayscale image
 #       = [1] for color image
 def inputImage(fileName,colour = 1):
-    return cv2.imread(fileName,colour)
+    return cv2.imread("testWalls/"+fileName,colour)
 
 ## Displays image in seperate window
 # cv2 img = cv2 object of image
@@ -55,7 +56,7 @@ def Threshold_Basic (Image,Threshold,Output_Pixel=255):
 
 def Threshold (Image,Output_Pixel=255):
     T_Value,Threshold_Image = cv2.threshold (Image,0,Output_Pixel,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-    return T,Threshold_Image
+    return T_Value,Threshold_Image
 
 def Mask (Input_Image, Mask):
     Masked_Image = cv2.bitwise_and(Input_Image,Input_Image,mask = Mask)
@@ -114,9 +115,10 @@ def Find_Contours_Optimal_Moment (Image,Target_Image,Filled = True):
     for H in Hulls:
         M = cv2.moments(H)
         
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
+        if (int(M["m00"]))!= 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
         
-        cv2.circle(Target_Image, (cX, cY), 5, (0, 0, 255), -1)
+            cv2.circle(Target_Image, (cX, cY), 5, (0, 0, 255), -1)
         
     return Target_Image

cb39f9fc2346457d63f2d72efc4f46715dfa3b2d
Paulettechee <99034719+Paulettechee@users.noreply.github.com>
2022-03-05 17:55:02 -0500

    Update BetaBuddy_TwoPaths_cos.py

--- Beta Code/BetaBuddy_TwoPaths_cos.py
+++ Beta Code/BetaBuddy_TwoPaths_cos.py
@@ -41,6 +41,7 @@ class Hold:
         self.colour = RED
         self.close = []
         self.twohands = False
+        self.foot_holds = []
         
     def get_position(self):
         return self.x, self.y
@@ -70,7 +71,6 @@ class Hold:
     def draw_edge(self, window, p):
         pygame.draw.line(window, ORANGE, (self.x, self.y), p.get_posistion())
     
-    # Currently adds self to neighbour list
     def update_neighbors(self, holdsList):
         self.neighbors = []
         for hold in holdsList:
@@ -79,6 +79,15 @@ class Hold:
                 if abs(self.x - x) < 125 and abs(self.y - y) < 125 and (y - self.y) < 0:
                     self.neighbors.append(hold)
                     #hold.make_neighbour()
+    
+    def update_feet(self, holdsList):
+        self.foot_holds = []
+        for hold in holdsList:
+            if hold != self:
+                x,y = hold.get_position()
+                if y > self. y and abs(self.y - y) < 350 and abs(self.y - y) > 150: # abs(self.x - x) < 125 and abs(self.y - y) < 125 and (y - self.y) < 0:
+                    self.foot_holds.append(hold)
+#                    hold.make_neighbor()
     
     def is_close(self,holdsList):
         # for path1 x = 25 y = 35
@@ -139,7 +148,16 @@ def printHoldNumber(window,path,font):
         text = str(key_list[i])
         printFont(window,font,text,x,y)
         
-    
+def update_neighbours_feet(classHoldsList):
+    for i in classHoldsList:
+        i.update_neighbors(classHoldsList)
+        i.update_feet(classHoldsList)
+
+def draw_paths(colours,paths,screen):
+    for j, path in enumerate(paths):
+            col = colours[j]
+            drawPath(screen,path,col) 
+            
 # Hurestic function (Are we getting closer to the goal?)
 def heuristic(p1, p2):  #p1, p2 are cooridantes ie (x,y)
 	x1, y1 = p1.get_position()
@@ -152,14 +170,18 @@ def g(p1, p2):  #p1, p2 are cooridantes ie (x,y)
 	x2, y2 = p2.get_position()
 	return math.sqrt((x1 - x2)**2 + (y1 - y2)**2) # Eculidan Distance
 
-def find_paths(start_left, start_right, end, holdList):
+def find_paths(start_left, start_right, start_foot_left, start_foot_right, end, holdList):
     
 
-    left_path = {1: start_left}#Path(start_left)
-    right_path = {1: start_right}#Path(start_right)
+    left_path = {1: start_left}
+    right_path = {1: start_right}
+    
+    left_foot_path = {1: start_foot_left}
+    right_foot_path = {1: start_foot_right}
     
     #Define list of all paths
     paths = [left_path, right_path]
+    foot_paths = [left_foot_path, right_foot_path]
     
     last_hold_in_path = start_left
     count = 1
@@ -215,16 +237,132 @@ def find_paths(start_left, start_right, end, holdList):
         currentPath = paths[1]
         currentPath[count] = end        
     
-    return paths
+    return paths, foot_paths
+  
 
-def update_neighbours(classHoldsList):
-    for i in classHoldsList:
-        i.update_neighbors(classHoldsList)
+def get_left_right_feet(feet):
+    
+    if feet[0].x < feet[1].x:
+        left = feet[0]
+        right = feet[1]
+    else:
+        left = feet[1]
+        right = feet[0]
+        
+    return left, right
 
-def draw_paths(colours,paths,screen):
-    for j, path in enumerate(paths):
-            col = colours[j]
-            drawPath(screen,path,col)   
+def max_dist_feet(pos_feet):
+    max_dist = -1
+    foot_list = []
+    
+    for i in pos_feet:
+        for j in pos_feet:
+            x1,y1 = i.get_position()
+            x2,y2 = j.get_position()
+            
+            dist = abs(x1-x2)
+            
+            if dist > max_dist:
+                max_dist = dist
+                hold1 = i
+                hold2 = j
+    
+    foot_list = [hold1,hold2]
+    
+    return foot_list
+
+def get_feet(hand_paths, start_foot_left, start_foot_right):
+    
+    left_foot_path = {1: start_foot_left}
+    right_foot_path = {1: start_foot_right}
+    
+    count = 2
+    
+    # make copy of left  hand path
+    all_hands = hand_paths[0].copy()
+    
+    # Merge left and right hands
+    all_hands.update(hand_paths[1])
+    
+    # Loop through all hands after start hold
+    for i in range(2,len(all_hands)+1):
+        # Get currnt hand
+        current_hand = all_hands[i]
+        
+        # Get possible feet and current feet to determine if they should move
+        pos_feet = current_hand.foot_holds
+        current_left_foot = list(left_foot_path.values())[-1]
+        current_right_foot = list(right_foot_path.values())[-1]
+        
+        # If current foot is not in possible feet foot needs to move
+        is_left = not(current_left_foot in pos_feet)
+        is_right = not(current_right_foot in pos_feet)
+        
+        # If both left and right are not in pos_feet
+        if is_left and is_right:
+            # If left and right are not in pos feet
+            if len(pos_feet) == 2: # If only two feet options put left to left and right ti right
+                left, right = get_left_right_feet(pos_feet)
+                left_foot_path[count] = left
+                right_foot_path[count] = right
+                count +=1 
+                
+            else:
+                # Find max x disatnce between possible feet for best stability
+                two_foots = max_dist_feet(pos_feet)
+                
+                # Pick left and right
+                left, right = get_left_right_feet(two_foots)
+                left_foot_path[count] = left
+                right_foot_path[count] = right
+                count +=1
+        
+        elif is_left:
+            pos_feet.remove(current_right_foot)
+            
+            if len(pos_feet) == 2: # If only two feet options put left to left and right ti right
+                left, right = get_left_right_feet(pos_feet)
+                left_foot_path[count] = left
+                right_foot_path[count] = right
+                count +=1
+                
+            else:
+                # Find max x disatnce between possible feet for best stability
+                two_foots = max_dist_feet(pos_feet)
+                
+                # Pick left and right
+                left, right = get_left_right_feet(two_foots)
+                left_foot_path[count] = left
+                right_foot_path[count] = right
+                count +=1
+        
+        elif is_right:
+            pos_feet.remove(current_left_foot)
+            if len(pos_feet) == 2: # If only two feet options put left to left and right ti right
+                left, right = get_left_right_feet(pos_feet)
+                left_foot_path[count] = left
+                right_foot_path[count] = right
+                count +=1
+                
+            else:
+                # Find max x disatnce between possible feet for best stability
+                two_foots = max_dist_feet(pos_feet)
+                
+                # Pick left and right
+                left, right = get_left_right_feet(two_foots)
+                left_foot_path[count] = left
+                right_foot_path[count] = right
+                count +=1
+        
+    return left_foot_path, right_foot_path
+        
+#    left_foot_path = {1: start_foot_left}
+#    right_foot_path = {1: start_foot_right}
+#    
+#    foot_paths = [left_foot_path, right_foot_path]
+    
+    pass
+    
 
 def main():
     # Screen Setup
@@ -242,25 +380,47 @@ def main():
     start = classHoldsList[3]
     start.make_start()
     
+    
+    left_foot = classHoldsList[0]
+    right_foot = classHoldsList[1]
+    
+    print(start.get_position())
+    print(classHoldsList[2].get_position())
+    print(left_foot.get_position())
+    print(right_foot.get_position())
+    
+    classHoldsList[13].update_feet(classHoldsList)
+    
     classHoldsList[8].twohands = True
     
     end = classHoldsList[-1]
     end.make_end()
     
     # Get neighbours for each hold
-    update_neighbours(classHoldsList)
+    update_neighbours_feet(classHoldsList)
     
     # Find paths
-    paths = find_paths(start, start, end, classHoldsList)
+    hand_paths, foot_paths = find_paths(start, start, left_foot, right_foot, end, classHoldsList)
 #    print(paths)
-    print(list(paths[0]))
-    print(list(paths[1]))
+    print(list(hand_paths[0]))
+    print(list(hand_paths[1]))
+    
+    
+    left_feet, right_feet = get_feet(hand_paths, left_foot, right_foot)
+    
+    print(left_feet)
+    print(right_feet)
+    
+    feet_paths = [left_feet,right_feet]
     
     pygame.init() # intilize font
     font=pygame.font.SysFont('helvetica',20) #define the font and size
     
-#    printHoldNumber(screen,paths[0],font)
-    printHoldNumber(screen,paths[1],font)
+#    printHoldNumber(screen,hand_paths[0],font)
+#    printHoldNumber(screen,hand_paths[1],font)
+    
+    printHoldNumber(screen,feet_paths[0],font)
+    printHoldNumber(screen,feet_paths[1],font)
     
 #    for i in paths[1].holds:
 #        i.make_neighbor()
@@ -279,7 +439,8 @@ def main():
         
         #Draw Paths
         colours = [PURPLE, GREY]
-        draw_paths(colours,paths,screen)
+#        draw_paths(colours,hand_paths,screen)
+        draw_paths(colours,feet_paths,screen)
     
     
     pygame.quit()
